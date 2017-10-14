00a80eedcaaa77dfeed813afc75750117c021825
Alexandru-Paul Copil (thee-engineer) <alexandru.p.copil@gmail.com>
2017-10-14 12:43:03 +0100

    Update api, switching to JS

--- apitest.py
+++ apitest.py
@@ -1,5 +1,4 @@
 import requests
-import datetime
 from pprint import pprint
 
 API = 'https://api.github.com/%s'
@@ -10,15 +9,26 @@ def help():
     return requests.get(API % '', HEADER)
 
 
-def list_repos(user):
+def user_list(id):
+    r = requests.get(API % 'users?since=%d' % id, HEADER).json()
+    print r
+
+
+def user_list_repos(user):
     return requests.get(API % 'users/%s/repos' % user, HEADER)
 
 
-def search_repo(query):
+def repo_search(query):
     return requests.get(API % 'search/repositories?q=%s' % query, HEADER)
+
+
+def repo_languages(user, repo):
+    return requests.get(API % 'repos/%s/%s/languages' % (user, repo), HEADER)
 
 
 if __name__ == '__main__':
     # pprint(help().json())
     # print list_repos('thee-engineer').json()
-    pprint(search_repo('sheffield').json())
+    # pprint(search_repo('sheffield').json())
+    # pprint(repo_languages('thee-engineer', 'cryptor').json())
+    user_list(145)
