743e3e57180298c3a83c2c6caa8fddb871ba8dd0
bogdan23a <gbogdan23a@yahoo.com>
2017-10-15 05:31:38 +0300

    update

--- our_lord_and_saviour_python/app.py
+++ our_lord_and_saviour_python/app.py
@@ -30,7 +30,7 @@ def handle_data():
             ghuser.active_matches = MATCHED_PROFILES[username][1]
 
         # , username=ghuser.username, location=ghuser.location, bio=ghuser.bio, repocount=ghuser.repo_count, l1=ghuser.lang_name[0], l2=ghuser.lang_name[1], l3=ghuser.lang_name[2]
-        return render_template('app/profile.html', user_name=ghuser.username, location=ghuser.location, bio=ghuser.bio, repo_count=ghuser.repo_count, lang0=ghuser.lang_name[0][0], lang1=ghuser.lang_name[1][0], lang2=ghuser.lang_name[2][0], match_count=len(ghuser.matches)-ghuser.mindex, avatar_url=ghuser.avatar)
+        return render_template('app/profile.html', user_name=ghuser.username, location=ghuser.location, bio=ghuser.bio, repo_count=ghuser.repo_count, lang0=ghuser.lang_name[0][0], lang1=ghuser.lang_name[1][0], lang2=ghuser.lang_name[2][0], match_count=len(ghuser.active_matches), avatar_url=ghuser.avatar)
     elif request.method == 'GET':
         return render_template('app/index.html')
     else:
@@ -42,7 +42,7 @@ def user():
     global ghuser
     if ghuser is None:
         return render_template('app/index.html')
-    return render_template('app/profile.html', user_name=ghuser.username, location=ghuser.location, bio=ghuser.bio, repo_count=ghuser.repo_count, lang0=ghuser.lang_name[0][0], lang1=ghuser.lang_name[1][0], lang2=ghuser.lang_name[2][0], match_count=len(ghuser.matches)-ghuser.mindex, avatar_url=ghuser.avatar)
+    return render_template('app/profile.html', user_name=ghuser.username, location=ghuser.location, bio=ghuser.bio, repo_count=ghuser.repo_count, lang0=ghuser.lang_name[0][0], lang1=ghuser.lang_name[1][0], lang2=ghuser.lang_name[2][0], match_count=len(ghuser.active_matches), avatar_url=ghuser.avatar)
 
 
 @app.route('/explore', methods=['GET', 'POST'])
@@ -62,7 +62,6 @@ def dislike_love():
             if MATCHED_PROFILES.get(muser, None) is None:
                 MATCHED_PROFILES[muser] = ([ghuser.username], [])
             elif muser in MATCHED_PROFILES[ghuser.username][0]:
-                print '******* ******* SHITFUCK ******* *******'
                 MATCHED_PROFILES[muser][1].append(ghuser.username)
                 MATCHED_PROFILES[ghuser.username][1].append(muser)
         else:
